c6a5fe8b030fbd90457f89fd7d08a35959cd2ac5
DerekBlackD <knljnr@gmail.com>
2024-04-12 00:48:56 -0500

    Loop for

--- src/main.go
+++ src/main.go
@@ -2,29 +2,23 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int {
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola mundo")
-	tripleArgument(1, 2, "Hola")
+	// for condicional
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+	}
 
-	value := returnValue(2)
-	fmt.Println("Value:", value)
+	// for while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
 
-	value1, _ := doubleReturn(2)
-	println(value1)
+	// for forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 }
