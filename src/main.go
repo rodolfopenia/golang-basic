3f8d15e0507cfb9dd912e933cb6364764a1f70b6
DerekBlackD <knljnr@gmail.com>
2024-04-12 01:25:15 -0500

    resolve excersise

--- src/main.go
+++ src/main.go
@@ -2,23 +2,37 @@ package main
 
 import "fmt"
 
+func evaluateNumber(number int) string {
+	var result string
+	if number%2 == 0 {
+		result = "par"
+	} else {
+		result = "impar"
+	}
+	return result
+}
+
+func login(user, password string) bool {
+	var result bool
+	if user == "user" && password == "123" {
+		result = true
+	} else {
+		result = false
+	}
+	return result
+}
+
 func main() {
-	// for condicional
-	for i := 0; i <= 10; i++ {
-		fmt.Println(i)
-	}
+	const number int = 7
+	result := evaluateNumber(number)
+	fmt.Printf("El número %d es %s\n", number, result)
 
-	// for while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
-	}
-
-	// for forever
-	counterForever := 0
-	for {
-		fmt.Println(counterForever)
-		counterForever++
+	const user string = "user"
+	const password string = "1234"
+	login := login(user, password)
+	if login {
+		fmt.Println("Login exitoso")
+	} else {
+		fmt.Println("Error al realizar login")
 	}
 }
