c9ff7fa343e2aed2c01903b737025651d0ff992f
DerekBlackD <knljnr@gmail.com>
2024-04-12 00:16:27 -0500

    Learn package fmt

--- src/main.go
+++ src/main.go
@@ -3,56 +3,23 @@ package main
 import "fmt"
 
 func main() {
+	helloMessage := "Hello"
+	worldMessage := "World"
 
-	const squareBase = 10
-	squareArea := squareBase * squareBase
-	fmt.Println("Area cuadrado:", squareArea)
+	// printLn - Add line break
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
 
-	x := 10
-	y := 50
+	// printF
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
 
-	// Add
-	result := x + y
-	fmt.Println("Suma:", result)
+	// Sprintf
+	message := fmt.Sprintf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Println(message)
 
-	// Rest
-	result = y - x
-	fmt.Println("Resta:", result)
-
-	// Multiply
-	result = x * y
-	fmt.Println("Multiplicar", result)
-
-	// Split
-	result = y / x
-	fmt.Println("División: ", result)
-
-	// Residue
-	result = y % x
-	fmt.Println("Residuo:", result)
-
-	// Increment
-	x++
-	fmt.Println("Incremental:", x)
-
-	// Decrement
-	x--
-	fmt.Println("Decremental", x)
-
-	// Homework
-	// Find the area of rectangle, trapeze and circle.
-	const base float64 = 10
-	const height float64 = 5
-	const minorBase float64 = 6
-	const majorBase float64 = 8
-	const PI float64 = 3.14
-	const diameter float64 = 6
-
-	rectangleArea := base * height
-	trapezeArea := ((majorBase + minorBase) / 2) * height
-	circleArea := diameter * PI
-
-	fmt.Println("Área del rectángulo:", rectangleArea)
-	fmt.Println("Área del trapecio:", trapezeArea)
-	fmt.Println("Área del círculo:", circleArea)
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("Cursos: %T\n", cursos)
 }
