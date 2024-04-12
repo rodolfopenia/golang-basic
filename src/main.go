648a0bbf2e1c7a47a08f9de2bc66df58f17a3022
DerekBlackD <knljnr@gmail.com>
2024-04-12 17:10:04 -0500

    Range

--- src/main.go
+++ src/main.go
@@ -1,31 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindromo(text string) {
+	var textReverse string
+	text = strings.ToLower(text)
+
+	for i := len(text) - 1; i >= 0; i-- {
+		textReverse += string(text[i])
+	}
+
+	if text == textReverse {
+		fmt.Println("Es palíndromo")
+	} else {
+		fmt.Println("No es palíndromo")
+	}
+}
 
 func main() {
 
-	// Array - Son inmutables
-	var arr [4]int
-	arr[0] = 1
-	arr[1] = 2
-	fmt.Println(arr, len(arr), cap(arr))
+	slice := []string{"hola", "que", "hace"}
 
-	// Slice
-	slice := []int{0, 1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
+	for _, valor := range slice {
+		fmt.Println(valor)
+	}
 
-	// Methods slice
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append new slice
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	isPalindromo("Ama")
 }
