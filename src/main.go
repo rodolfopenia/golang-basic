3055be832777027327c5805db98442cc18e3c27b
DerekBlackD <knljnr@gmail.com>
2024-04-12 18:59:01 -0500

    Pointers and Structs

--- src/main.go
+++ src/main.go
@@ -1,20 +1,43 @@
 package main
 
 import (
-	pk "curso-golang/src/mypackage"
 	"fmt"
 )
 
-type car struct {
+type pc struct {
+	ram   int
+	disk  int
 	brand string
-	year  int
+}
+
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "pong")
+}
+
+func (myPC *pc) duplicateRAM() {
+	myPC.ram = myPC.ram * 2
 }
 
 func main() {
-	var myCar pk.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2020
-	fmt.Println(myCar)
+	a := 50
+	b := &a
 
-	pk.PrintMessage("Hola PLatzi")
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	*b = 100
+	fmt.Println(a)
+
+	myPC := pc{ram: 16, disk: 200, brand: "msi"}
+	fmt.Println(myPC)
+
+	myPC.ping()
+
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+
+	fmt.Println(myPC)
 }
