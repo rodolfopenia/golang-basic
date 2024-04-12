ba6d01331879e49b43fd55fc48c6335a897d6ded
DerekBlackD <knljnr@gmail.com>
2024-04-12 16:57:52 -0500

    array and slices

--- src/main.go
+++ src/main.go
@@ -3,22 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// Defer - execute to end
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
 
-	// Continue and Break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	// Array - Son inmutables
+	var arr [4]int
+	arr[0] = 1
+	arr[1] = 2
+	fmt.Println(arr, len(arr), cap(arr))
 
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	// Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
-	}
+	// Methods slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append new slice
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
