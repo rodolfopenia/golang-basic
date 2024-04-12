9a0cd3e430b07c54d1ee3a3242527fe67583e73a
DerekBlackD <knljnr@gmail.com>
2024-04-12 01:34:08 -0500

    switch

--- src/main.go
+++ src/main.go
@@ -2,37 +2,23 @@ package main
 
 import "fmt"
 
-func evaluateNumber(number int) string {
-	var result string
-	if number%2 == 0 {
-		result = "par"
-	} else {
-		result = "impar"
-	}
-	return result
-}
-
-func login(user, password string) bool {
-	var result bool
-	if user == "user" && password == "123" {
-		result = true
-	} else {
-		result = false
-	}
-	return result
-}
-
 func main() {
-	const number int = 7
-	result := evaluateNumber(number)
-	fmt.Printf("El número %d es %s\n", number, result)
+	module := 8 % 2
 
-	const user string = "user"
-	const password string = "1234"
-	login := login(user, password)
-	if login {
-		fmt.Println("Login exitoso")
-	} else {
-		fmt.Println("Error al realizar login")
+	switch module {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
+	}
+
+	value := 200
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor de 0")
+	default:
+		fmt.Println("No condición")
 	}
 }
